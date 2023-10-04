3ae0f2403085265414017508be441e4643f8bdef
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2023-10-04 11:08:49 +0200

    This bunny should be killed

--- js/index.js
+++ js/index.js
@@ -24,10 +24,35 @@ const display = document.querySelector('[data-js="display"]');
 
 function getGreeting() {
   // Code here
+  const currentHour = new Date().getHours();
+  // console.log({currentHour});
+  if (currentHour >= 6 && currentHour <= 12){
+    return `Good Morning, It is ${currentHour} O\'clock`
+  } else if (currentHour >= 13 && currentHour <= 18) {
+    return `Good Afternoon, It is ${currentHour} O\'clock`
+  }else if (currentHour >= 19 && currentHour <= 22) {
+    return `Good Evening, It is ${currentHour} O\'clock`
+  }else if (currentHour >= 23 && currentHour <= 5) {
+    return `Good Night, It is ${currentHour} O\'clock`
+  }
+  return currentHour
 }
 
 function getDayColor() {
   // Code here
+  let day = new Date().getDay();
+  // console.log({day})
+  switch(day) {
+    case 1 : return "darkgray";
+    break;
+    case 2 : return "lightblue";
+    break;
+    case 3 : return "hotpink";
+    break;
+    case 4 : return "darkgray";
+    break;
+  }
+  return "white"
 }
 
 display.textContent = getGreeting();
